6e3c4fdd472915509d22add942ba515061e31cad
wayan29 <44164208+wayan29@users.noreply.github.com>
2019-03-04 17:07:22 +0800

    Add files via upload

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+// configurasi //
+
+
+// user_name boleh kosong //
+$user_name="bebas";
+
+//user_gaid wajib isi
+$user_gaid= "f4d48ab6-ceda-405f-9e76-2ab08b143432";
+//user_token wajib di isi //
+$user_token="AbwqAHXJIJB8I6TO";
+
+//user_id wajib di isi //
+$user_id="Facebook_144357279905318";
+
+// invite_code wajib di isi //
+$invite_code="FezUQt2FK";
+
+// Bearer wajib di isi //
+$Bearer="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOiJGYWNlYm9va18xNDQzNTcyNzk5MDUzMTgiLCJleHAiOiIxNTQ4NTQ4MTMxMTExIn0.WelLnLDksogZDrU7mbVCOluADwoDM4zB4S5xj1EEoXg";
+
+
+
+?>
